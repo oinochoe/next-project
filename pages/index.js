9eeccb9182365a47c1851712cc9bb9e3478fbb53
Yeongmin Kim <oinochoe@naver.com>
2019-10-27 14:48:48 +0900

    nextjs 시작해보기

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Layout from "../components/Layout";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+
+const ProfileLink = props => (
+  <div>
+    <Link href={`/p/[profile]`} as={`/p/${props.profile}`}>
+      <a>Go to {props.profile}'s profile</a>
+    </Link>
+  </div>
+);
+
+const Index = props => (
+  <Layout>
+    <h1>Friends List {props.profiles[0]}</h1>
+    {props.profiles.map((profile, index) => (
+      <ProfileLink key={index} profile={profile} />
+    ))}
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch("https://uinames.com/api/?amount=10");
+  const data = await res.json();
+
+  return {
+    profiles: data.map(profile => profile.name)
+  };
+};
+
+export default Index;
